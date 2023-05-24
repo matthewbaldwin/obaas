50f704abfa5e2591025bc0c6eb0a592c449ebc23
Matthew.Baldwin <matthew.baldwin@oracle.com>
2023-05-24 15:39:38 +0000

    Changes to be committed: 	modified:   accounts/src/main/java/com/example/accounts/controller/AccountsController.java

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -1,16 +1,19 @@
 package com.example.accounts.controller;
 
-
-
 import com.example.accounts.repository.AccountRepository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.ArrayList;
 import com.example.accounts.model.Account;
-
 
 @RestController
 @RequestMapping("/api/v1")
@@ -26,6 +29,19 @@ public class AccountsController {
     public List<Account> getAllAccounts() {
         return accountRepository.findAll();
     }
+
+    @GetMapping("/account/{accountId}")
+    public ResponseEntity<Account> getAccountById(@PathVariable("accountId") long accountId) {
+        Optional<Account> accountData = accountRepository.findById(accountId);
+        try {
+            return accountData.map(account -> new ResponseEntity<>(account, HttpStatus.OK))
+                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    
 
     @GetMapping("/hello")
     public String ping() {
