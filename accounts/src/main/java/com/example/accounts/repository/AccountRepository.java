460247fb4f377b4d35cba4e1803730c33ec3b12e
Matthew.Baldwin <matthew.baldwin@oracle.com>
2023-05-24 17:17:59 +0000

    modified:   accounts/src/main/java/com/example/accounts/repository/AccountRepository.java

--- accounts/src/main/java/com/example/accounts/repository/AccountRepository.java
+++ accounts/src/main/java/com/example/accounts/repository/AccountRepository.java
@@ -2,6 +2,7 @@ package com.example.accounts.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.accounts.model.Account;
+import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {   
    List<Account> findByAccountCustomerId(String customerId);
