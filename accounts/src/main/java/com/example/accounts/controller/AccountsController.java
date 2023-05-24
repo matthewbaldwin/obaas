45720d4ee091748ec303961fbb8edbadc46809cf
Matthew Baldwin <matthewbaldwin@comcast.net>
2023-05-24 13:37:33 -0400

    Update AccountsController.java
    Added create account that was mistakenly delated

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -24,6 +24,17 @@ public class AccountsController {
     public AccountsController(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
+    
+    @PostMapping("/account")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+    try {
+        Account _account = accountRepository.saveAndFlush(account);
+        return new ResponseEntity<>(_account, HttpStatus.CREATED);
+    } catch (Exception e) {
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
 
     @GetMapping("/accounts")
     public List<Account> getAllAccounts() {
